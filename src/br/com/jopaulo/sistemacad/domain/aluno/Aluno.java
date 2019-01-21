9d85939fdc41475b4791866fc483e81b013a51a3
João Paulo Mendes <jp_cbc@hotmail.com>
2019-01-21 16:13:34 -0300

    Enviando o primeiro commit

---
+++ src/br/com/jopaulo/sistemacad/domain/aluno/Aluno.java
@@ -0,0 +1,109 @@
+package br.com.jopaulo.sistemacad.domain.aluno;
+
+import java.time.LocalDate;
+
+public class Aluno {
+	
+	public enum Situacao{
+		Ativo, Inativo, Pendente;
+	}
+	public enum Sexo{
+		Masculino, Feminino;
+	}
+	private String codigo;
+	private String nome;
+	private Sexo sexo;
+	private Integer rg;
+	private Integer cpf;
+	private LocalDate dataNascimento;
+	private Situacao situacao;
+	private Contato contato = new Contato();
+	private Endereco endereco = new Endereco();
+	
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Sexo getSexo() {
+		return sexo;
+	}
+	public void setSexo(Sexo sexo) {
+		this.sexo = sexo;
+	}
+	public Integer getRg() {
+		return rg;
+	}
+	public void setRg(Integer rg) {
+		this.rg = rg;
+	}
+	public Integer getCpf() {
+		return cpf;
+	}
+	public void setCpf(Integer cpf) {
+		this.cpf = cpf;
+	}
+	public LocalDate getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(LocalDate dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+	public Situacao getSituacao() {
+		return situacao;
+	}
+	public void setSituacao(Situacao situacao) {
+		this.situacao = situacao;
+	}
+	public Contato getContato() {
+		return contato;
+	}
+	public void setContato(Contato contato) {
+		this.contato = contato;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	@Override
+	public String toString() {
+		return "Aluno [codigo=" + codigo + ", nome=" + nome + ", sexo=" + sexo + ", rg=" + rg + ", cpf=" + cpf
+				+ ", dataNascimento=" + dataNascimento + ", situacao=" + situacao + ", contato=" + contato
+				+ ", endereco=" + endereco + "]";
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+}
---
+++ src/br/com/jopaulo/sistemacad/domain/aluno/Contato.java
@@ -0,0 +1,93 @@
+package br.com.jopaulo.sistemacad.domain.aluno;
+
+public class Contato {
+	
+	private String email;
+	private Integer dddCelular;
+	private Integer numeroCelular;
+	private Integer dddfixo;
+	private Integer numeroFixo;
+	
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public Integer getDddCelular() {
+		return dddCelular;
+	}
+	public void setDddCelular(Integer dddCelular) {
+		this.dddCelular = dddCelular;
+	}
+	public Integer getNumeroCelular() {
+		return numeroCelular;
+	}
+	public void setNumeroCelular(Integer numeroCelular) {
+		this.numeroCelular = numeroCelular;
+	}
+	public Integer getDddfixo() {
+		return dddfixo;
+	}
+	public void setDddfixo(Integer dddfixo) {
+		this.dddfixo = dddfixo;
+	}
+	public Integer getNumeroFixo() {
+		return numeroFixo;
+	}
+	public void setNumeroFixo(Integer numeroFixo) {
+		this.numeroFixo = numeroFixo;
+	}
+	@Override
+	public String toString() {
+		return "Contato [email=" + email + ", dddCelular=" + dddCelular + ", numeroCelular=" + numeroCelular
+				+ ", dddfixo=" + dddfixo + ", numeroFixo=" + numeroFixo + "]";
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dddCelular == null) ? 0 : dddCelular.hashCode());
+		result = prime * result + ((dddfixo == null) ? 0 : dddfixo.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + ((numeroCelular == null) ? 0 : numeroCelular.hashCode());
+		result = prime * result + ((numeroFixo == null) ? 0 : numeroFixo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Contato other = (Contato) obj;
+		if (dddCelular == null) {
+			if (other.dddCelular != null)
+				return false;
+		} else if (!dddCelular.equals(other.dddCelular))
+			return false;
+		if (dddfixo == null) {
+			if (other.dddfixo != null)
+				return false;
+		} else if (!dddfixo.equals(other.dddfixo))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (numeroCelular == null) {
+			if (other.numeroCelular != null)
+				return false;
+		} else if (!numeroCelular.equals(other.numeroCelular))
+			return false;
+		if (numeroFixo == null) {
+			if (other.numeroFixo != null)
+				return false;
+		} else if (!numeroFixo.equals(other.numeroFixo))
+			return false;
+		return true;
+	}	
+}
